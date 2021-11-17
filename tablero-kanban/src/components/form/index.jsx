17d07c9d88b5789dcb1ed91efe6fb8aa944e1b74
Pedro <pedro.matas.ortega@gmail.com>
2021-11-17 18:06:49 +0100

    subido el form sin mergear con el task

--- tablero-kanban/src/components/form/index.jsx
+++ tablero-kanban/src/components/form/index.jsx
@@ -5,50 +5,61 @@ import { useEffect } from 'react';
 
 function Form() {
     const [task, setTask] = useState([]);
- let [cont,updatecont]= useState(0);
+    const [btnEnabled, setBtnEnabled] = useState(false);
+
+ let counter = localStorage.getItem('counter') ?? 1;
+ let [cont,updatecont]= useState(counter);
+ console.log(counter)
     useEffect (() => {
         let arr = [];
-        for (let i =1; i<cont;i++){
+        for (let i =1; i<counter;i++){
                 if (JSON.parse(localStorage.getItem(`task${cont}`)!== null)){
                     arr.push(JSON.parse(localStorage.getItem(`task${i}`)))
-                }
+                }console.log(arr)
         }
+        
        setTask(arr)
     }, [cont])
-
-   let [texto,updateText]= useState('');
   
 
-  const  textoVacio=e =>{
-        if(e.target.defaultValue===""){
-           
+
+    const textoVacio=(e) =>{
+        if(e.target.value===""){
+            setBtnEnabled(false);
+        } else {
+            setBtnEnabled(true);
         }
+        
     }
-  
     const enviar= (e)=>{
         e.preventDefault();
+
+        const day = new Date();
+        const fecha = `${day.getDate()}/${day.getMonth() + 1 }/${day.getFullYear()} ${day.getHours()}:${day.getMinutes()}:${day.getSeconds()}`;
         updatecont(cont+1)
     const tarea={
       id:cont,
-      tarea: e.target.text.value
+      tarea: e.target.text.value,
+      fecha:fecha
       
   }
     
   localStorage.setItem(`task${cont}`, JSON.stringify(tarea));
+  counter++;
+  localStorage.setItem('counter',counter)
     }
-    const pintar=()=>{
-      localStorage.getItem(`task${cont}`)
-      
-  }
+   
 
     return (
         <Fragment>
         <form action="" onSubmit={enviar}>
           <textarea name="text" placeholder="Enter a note" onInput={textoVacio}></textarea>
           <div>{task}</div>
-          <div>
-              <button type="submit">Add</button>
-              <button type="reset">Cancel</button>
+          <div className="boton-container">
+          {btnEnabled ? <button type='submit' className='boton boton-submit'>Add</button>
+                            : <button type='submit' className='boton boton-submit btn_opacity'>Add</button>}
+             
+              <button type="reset" className="boton boton-reset" onClick={textoVacio}>Cancel</button>
           </div>
         </form>
         </Fragment>
